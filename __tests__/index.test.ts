2a9f03fbb096b16404250d94f9563c74d9ed8852
necessarylion <aj.zinkyaw@gmail.com>
2025-02-22 09:36:45 +0700

    increase timeout

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -8,18 +8,18 @@ describe('ThaiLotto', () => {
     expect(res?.data).toBeDefined()
     expect(res?.data.first.numbers[0]).toBe('847377')
     expect(res?.date).toBe(date)
-  })
+  }, 30000)
 
   it('should get array list greater than 0', async () => {
     const thaiLotto = new ThaiLotto()
     const res = await thaiLotto.getList(1)
     expect(res.length).toBeGreaterThan(0)
-  })
+  }, 30000)
 
   it('should get latest result', async () => {
     const thaiLotto = new ThaiLotto()
     const res = await thaiLotto.getLatest()
     expect(res?.data).toBeDefined()
     expect(res?.date).toBeDefined()
-  }, 10000)
+  }, 30000)
 })
